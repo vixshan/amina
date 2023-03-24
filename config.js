5ed6d922eee755ec08ddbf4c4d62905c2ab00385
Vikshan <103361575+vikkshan@users.noreply.github.com>
2023-03-24 10:08:24 +0000

    Changed main and start command to include pm2

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   SUPPORT_SERVER: "https://discord.gg/wMxe5PsnNg", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
-    DEFAULT_PREFIX: "$", // Default prefix for the bot
+    DEFAULT_PREFIX: "m!", // Default prefix for the bot
   },
   INTERACTIONS: {
     SLASH: true, // Should the interactions be enabled
